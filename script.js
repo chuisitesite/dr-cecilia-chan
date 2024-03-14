791774d6a787f3712c890e190e8b5e7890b22b48
chuisitesite <145745909+chuisitesite@users.noreply.github.com>
2024-03-14 21:58:38 +0800

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Get the modal
-var modal = document.getElementById('wechatModal');
+var wechatModal = document.getElementById("wechat-qr-modal");
 
 // Get the button that opens the modal
-var btn = document.getElementById('wechatLogo');
+var wechatBtn = document.getElementById("wechat-icon");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = 'block';
+wechatBtn.onclick = function() {
+    wechatModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = 'none';
+    wechatModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
+    if (event.target == wechatModal) {
+        wechatModal.style.display = "none";
     }
 }
